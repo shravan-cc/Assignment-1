b1a04b9baa0a974fe75b32b111a55f9fdbb5e24d
Shravan <shravan.hegde@codecraft.co.in>
2024-04-01 10:05:26 +0530

    Performed all the neccassary changes

--- prime.js
+++ prime.js
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 import assert from "assert";
-function isprime(num) {
-  //Pre-Conditions
 
+function isprime(num) {
+  // Pre-Conditions
   assert(typeof num === "number", "Argument must be a number");
   assert(num > 1, "Argument must be greater than 1");
   assert(Number.isInteger(num), "Argument must be an integer");
+
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
       return false;
@@ -13,23 +17,27 @@ function isprime(num) {
   return true;
 }
 
-assert(isprime(9) === false, "The given  number is not prime number");
-assert(isprime(11) === true, "The given  number is prime number");
-assert(isprime(9999991) === true, "The given  number is prime number");
-assert.throws(() => isprime("string"), Error);
-assert.throws(() => isprime(), Error);
-assert.throws(() => isprime(1), Error);
-assert.throws(() => isprime(-1), Error);
-assert.throws(() => isprime(4.87), Error);
+assert(isprime(9) === false, "9 is not a prime number");
+assert(isprime(11) === true, "11 is a prime number");
+assert(isprime(9999991) === true, "9999991 is a prime number");
+assert.throws(
+  () => isprime("string"),
+  Error,
+  "Invalid input should throw an error"
+);
+assert.throws(() => isprime(), Error, "Missing input should throw an error");
+assert.throws(() => isprime(1), Error, "1 is not a prime number");
+assert.throws(() => isprime(-1), Error, "Negative numbers are not prime");
+assert.throws(() => isprime(4.87), Error, "Non-integer numbers are not prime");
 
 function generatePrimeSeries(count) {
   let i = 2;
   let cnt = 0;
   let array = [];
-  if (typeof count !== "number" || !Number.isInteger(count)) {
-    throw new Error("Arguement must be a valid integer greater than 1");
-  }
+
+  assert(count > 0, "Argument must be a positive integer");
   assert(Number.isInteger(count), "Argument must be an integer");
+
   while (cnt !== count) {
     if (isprime(i)) {
       cnt++;
@@ -39,20 +47,73 @@ function generatePrimeSeries(count) {
   }
   return array;
 }
-assert.deepStrictEqual(
-  generateprimeseries(5),
-  [2, 3, 5, 7, 11],
-  "The first series"
-);
-assert.deepStrictEqual(
-  generatePrimeSeries(1),
-  [2],
-  "Generated prime series for count 1 is incorrect"
-);
 
+assert.deepEqual(
+  generatePrimeSeries(5),
+  [2, 3, 5, 7, 11],
+  "The first series should be generated correctly"
+);
+assert.deepEqual(
+  generatePrimeSeries(6),
+  [2, 3, 5, 7, 11, 13],
+  "The first series should be generated correctly"
+);
 assert.deepStrictEqual(
   generatePrimeSeries(10),
   [2, 3, 5, 7, 11, 13, 17, 19, 23, 29],
   "Generated prime series for count 10 is incorrect"
 );
-assert.throws(() => generatePrimeSeries(), Error);
+
+assert.throws(
+  () => generatePrimeSeries(),
+  Error,
+  "Missing input should throw an error"
+);
+assert.throws(
+  () => generatePrimeSeries(4.5),
+  Error,
+  "Missing input should throw an error"
+);
+
+console.log("All tests passed successfully!");
+=======
+=======
+>>>>>>> main
+=======
+>>>>>>> main
+function assert(bool, message) {
+    if (!bool) {
+        console.log(message);
+        
+        return;
+       
+    }
+}
+
+function isprime(num) {
+    //Pre-Conditions
+    assert(num > 0, `The given parameter must be a positive number`);
+    assert(Number.isInteger(num), `The given parameter ${num} must be an integer`);
+    assert(typeof num === 'number', `The given parameter ${num} must be a number`);
+    assert(num !== 0 || num !== 1, `The given parameter is neither prime nor composite`);
+
+    if (num > 1) {
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    } 
+}
+
+assert(isprime(9)===true,"The given  number is not prime number");
+assert(isprime(11)===false,"The given  number is prime number");
+isprime(4.2)
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> main
+=======
+>>>>>>> main
+=======
+>>>>>>> main
