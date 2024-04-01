d78196dcbd0b5f1bf7a38401fec232345ffac550
Shravan <shravan.hegde@codecraft.co.in>
2024-04-01 09:51:55 +0530

    Implemented camel case letters

--- prime.js
+++ prime.js
@@ -1,9 +1,7 @@
 import assert from "assert";
 function isprime(num) {
   //Pre-Conditions
-  if (typeof num !== "number" || num <= 1 || !Number.isInteger(num)) {
-    throw new Error("Arguement must be a valid integer greater than 1");
-  }
+
   assert(typeof num === "number", "Argument must be a number");
   assert(num > 1, "Argument must be greater than 1");
   assert(Number.isInteger(num), "Argument must be an integer");
@@ -17,13 +15,14 @@ function isprime(num) {
 
 assert(isprime(9) === false, "The given  number is not prime number");
 assert(isprime(11) === true, "The given  number is prime number");
+assert(isprime(9999991) === true, "The given  number is prime number");
 assert.throws(() => isprime("string"), Error);
 assert.throws(() => isprime(), Error);
 assert.throws(() => isprime(1), Error);
 assert.throws(() => isprime(-1), Error);
 assert.throws(() => isprime(4.87), Error);
 
-function generateprimeseries(count) {
+function generatePrimeSeries(count) {
   let i = 2;
   let cnt = 0;
   let array = [];
@@ -31,7 +30,6 @@ function generateprimeseries(count) {
     throw new Error("Arguement must be a valid integer greater than 1");
   }
   assert(Number.isInteger(count), "Argument must be an integer");
-  assert(typeof count === "number", "Argument must be a number");
   while (cnt !== count) {
     if (isprime(i)) {
       cnt++;
@@ -41,10 +39,20 @@ function generateprimeseries(count) {
   }
   return array;
 }
-//console.log(typeof([1,2,3,4,5]))
 assert.deepStrictEqual(
   generateprimeseries(5),
   [2, 3, 5, 7, 11],
   "The first series"
 );
-assert.throws(() => generateprimeseries(), Error);
+assert.deepStrictEqual(
+  generatePrimeSeries(1),
+  [2],
+  "Generated prime series for count 1 is incorrect"
+);
+
+assert.deepStrictEqual(
+  generatePrimeSeries(10),
+  [2, 3, 5, 7, 11, 13, 17, 19, 23, 29],
+  "Generated prime series for count 10 is incorrect"
+);
+assert.throws(() => generatePrimeSeries(), Error);
