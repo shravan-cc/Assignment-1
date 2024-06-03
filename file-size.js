f33dd4893e66160386b1514e64afebf1dcd8fe7d
Shravan R Hegde <163821861+shravan-cc@users.noreply.github.com>
2024-06-03 15:08:42 +0530

    Implemented async programming Assignment day-2

--- file-size.js
+++ file-size.js
@@ -128,3 +128,104 @@ export function getAllFilesASync(path, callback) {
     }
   });
 }
+/**
+ * Asynchronously retrieves the stats of a file or directory.
+ *
+ * @param {string} path - The path to the file or directory.
+ * @returns {Promise<fs.Stats>} A promise that resolves with the stats of the file or directory.
+ */
+export function promisifiedStat(path) {
+  return new Promise((resolved, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolved(stats);
+      }
+    });
+  });
+}
+/**
+ * Asynchronously reads the contents of a directory.
+ *
+ * @param {string} path - The path to the directory.
+ * @returns {Promise<string[]>} A promise that resolves with an array of filenames in the directory.
+ */
+export function promisifiedReaddir(path) {
+  return new Promise((resolved, reject) => {
+    fs.readdir(path, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolved(files);
+      }
+    });
+  });
+}
+/**
+ * Asynchronously retrieves the size of a file or directory using promises.
+ * If the path is a directory, it calculates the total size of all files recursively.
+ *
+ * @param {string} path - The path to the file or directory.
+ * @returns {Promise<number>} A promise that resolves with the size of the file or directory.
+ * @throws {Error} If an error occurs during file operations.
+ */
+export function sizeOfFileAtPathASyncUsingPromise(path) {
+  return promisifiedStat(path)
+    .then((stat) => {
+      if (stat.isFile()) return stat.size;
+      if (stat.isDirectory()) {
+        return promisifiedReaddir(path).then((files) => {
+          const promiseSizes = files.map((file) =>
+            sizeOfFileAtPathASyncUsingPromise(`${path}/${file}`)
+          );
+          return Promise.all(promiseSizes).then((sizes) =>
+            sizes.reduce((acc, size) => acc + size, 0)
+          );
+        });
+      }
+      return undefined;
+    })
+    .catch((err) => {
+      throw new Error(`Something went wrong:${err.message}`);
+    });
+}
+/**
+ * Asynchronously fetches the name of a person from the Star Wars API based on their ID.
+ *
+ * @param {number} personId - The ID of the person to fetch.
+ * @returns {Promise<string>} A promise that resolves with the name of the person.
+ * @throws {Error} If an error occurs during the fetch operation or if the response is invalid.
+ */
+export async function fetchPersonName(personId) {
+  const baseURL = "https://swapi.dev/api/people";
+  const url = `${baseURL}/${personId}`;
+  const response = await fetch(url);
+  if (response.status !== 200) {
+    throw new Error("Something went wrong, invalid response");
+  }
+  const data = await response.json();
+  return data.name;
+}
+/**
+ * Asynchronously fetches the names of people from the Star Wars API based on their IDs.
+ *
+ * @returns {Promise<Array<string>>} A promise that resolves with an array of person names.
+ * @throws {Error} If an error occurs during the fetch operation or if the response is invalid.
+ */
+export async function fetchPersonNamesByIds() {
+  const baseURL = "https://swapi.dev/api/people";
+  const personIds = [1, 2, 3];
+  const personNames = personIds.reduce(async (accPromise, id) => {
+    const acc = await accPromise;
+    const url = `${baseURL}/${id}`;
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      throw new Error("Something went wrong, invalid response");
+    }
+    const data = await response.json();
+    acc.push(data.name);
+    return acc;
+  }, []);
+  return personNames;
+}
